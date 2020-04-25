1fca41f7c1a8007857a4c4e093397599cdfbb858
Varun Kapoor <vkvarunkapoor1994@gmail.com>
2020-04-26 04:07:19 +0530

    First Commit

---
+++ src/main/java/io/spring/VarunPetClinicApplication.java
@@ -0,0 +1,13 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarunPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VarunPetClinicApplication.class, args);
+	}
+
+}
